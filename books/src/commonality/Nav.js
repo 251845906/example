53bb8bac753848008347ab6e7a662485d1b4d6ea
251845906 <251845906@qq.com>
2018-01-02 19:04:41 +0800

    18/01/02

--- books/src/commonality/Nav.js
+++ books/src/commonality/Nav.js
@@ -2,16 +2,10 @@ import React,{ Component } from 'react';
 import { Link } from 'react-router-dom';
 import './top.css';
 class Nav extends Component{
-    collectClick(){
-
-    }
-    telClick(){
-
-    }
     render(){
         return(
             <div className="nav clearfix">
-                <div className="container">
+                <div className="container clearfix">
                     <ul className="nav_item fl">
                         <li><Link to="/">首页</Link></li>
                         <li><Link to="/situation">本馆概况</Link></li>
@@ -20,9 +14,8 @@ class Nav extends Component{
                         <li><Link to="/resource">馆藏资源</Link></li>
                         <li><Link to="/exchange">学术交流</Link></li>
                     </ul>
-                    <div className="nav_rest">
-                        <span className="collect" onClick={this.collectClick.bind(this)}>收藏</span>
-                        <span className="tel" onClick={this.telClick.bind(this)}>电话</span>
+                    <div className="nav_rest fr">
+                        <span className="tel"><i class="iconfont icon-dianhua"></i>电话：000-000000</span>
                     </div>
                 </div>
             </div>
--- books/src/page/Index.js
+++ books/src/page/Index.js
@@ -1,10 +1,36 @@
-import React,{ Compoment } from 'react';
-
+import React,{ Component } from 'react';
+import { Link } from 'react-router-dom';
+import './css/index.css';
 
 const Index = () => {
     return(
-        <p>主页</p>
+        <div>
+            <Banner />
+        </div>
     )
+
 };
+class Banner extends Component{
+    render(){
+        return(
+            <div className="box">
+                <div className="banner"> </div>
+                <div className="container">
+                    <Link to="/"><img src={require('../images/logo.png')} alt="山西工商学院图书馆"/></Link>
+                    <Link to="/"><img src={require('../images/logo.png')} alt="山西工商学院图书馆"/></Link>
+                    <form action="">
+                        <select>
+                            <option value="作者">作者</option>
+                            <option value="书名">书名</option>
+                        </select>
+                        <input type="text"/>
+                        <input type="button" className="search iconfont" value="&#xe61f;检索"/>
+                        <input type="button" className="translate iconfont" value="&#xe620;中文英文"/>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Index;
