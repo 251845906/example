8aa648711f4b226812b8f560c689024f98ba5939
251845906 <251845906@qq.com>
2018-01-05 18:42:01 +0800

    18/01/05

--- books/src/page/Index.js
+++ books/src/page/Index.js
@@ -1,10 +1,13 @@
 import React,{ Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel } from 'antd'
+import { Carousel , Select } from 'antd'
+
+const Option = Select.Option;
 const Index = () => {
     return(
         <div className='box Index'>
             <Banner />
+            <Container />
         </div>
     )
 
@@ -22,27 +25,72 @@ class Banner extends Component{
                 <Carousel
                     autoplay={true}
                     autoplaySpeed={3000}
-                    arrows={true}
-                    focusOnSelect ={true}
+                    effect="fade"
+                    speed="500"
                 >
                     <div><div className="_banner_bj"><img src={require('../images/index_banner_01.png')} alt=""/></div></div>
                     <div><div className="_banner_bj"><img src={require('../images/index_banner_01.png')} alt=""/></div></div>
                     <div><div className="_banner_bj"><img src={require('../images/index_banner_01.png')} alt=""/></div></div>
                     <div><div className="_banner_bj"><img src={require('../images/index_banner_01.png')} alt=""/></div></div>
-
                 </Carousel>
                 <div className="container">
                     <Link to="/"><img src={require('../images/logo.png')} alt="山西工商学院图书馆"/></Link>
-                    <Link to="/"><img src={require('../images/logo.png')} alt="山西工商学院图书馆"/></Link>
+                    <Link to="/"><img src={require('../images/index_banner_text.png')} alt="山西工商学院图书馆"/></Link>
                     <form action="">
-                        <select>
-                            <option value="作者">作者</option>
-                            <option value="书名">书名</option>
-                        </select>
-                        <input type="text"/>
-                        <input type="button" className="search iconfont" value="&#xe61f;检索"/>
-                        <input type="button" className="translate iconfont" value="&#xe620;中文英文"/>
+                        <Select
+                            defaultValue="作家"
+                            style={{
+                                width: '108px',
+                                height:'40px',
+                                marginRight:'2px'
+                            }}
+
+                            dropdownClassName="Index_option"
+                            getPopupContainer={() => document.getElementsByClassName('banner')[0]}
+                        >
+                            <Option value="作家">作家</Option>
+                            <Option value="书名">书名</Option>
+                        </Select>
+                        <input type="text" placeholder="输入检索内容"/>
+                        <input type="button" className="search iconfont" value="&#xe61f; 检索"/>
+                        <input type="button" className="translate iconfont" value="&#xe620; 中文英文"/>
                     </form>
+                </div>
+            </div>
+        )
+    }
+}
+class Container extends Component{
+    render(){
+        return(
+            <div className="Index_container box">
+                <div className="notice">
+                    <div className="container">
+                        <span className="notice_title iconfont">公告 &#xe681;</span>
+                        <Carousel
+                            autoplay={true}
+                            autoplaySpeed={3000}
+                            vertical={true}
+                            dots={false}
+                        >
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                        </Carousel>
+                        <Carousel
+                            autoplay={true}
+                            autoplaySpeed={4000}
+                            vertical={true}
+                            dots={false}
+                        >
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                            <div><p><span className="dian">·</span><Link to="#">关于2017年期刊、报纸征订的通知。</Link></p></div>
+                        </Carousel>
+                        <span className="more"><Link to="#">MORE  >></Link></span>
+                    </div>
                 </div>
             </div>
         )
